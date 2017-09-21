8a0a52923f2a280a60e72bcf0941a6cf69c5ddcb
Purdy <374266@flourbluffschools.net>
2017-09-21 10:36:01 -0500

    class code

--- 1st-6Weeks/2017-09-20-Input/Convert.java
+++ 1st-6Weeks/2017-09-20-Input/Convert.java
@@ -1,3 +1,27 @@
 /** 
- * Converts centimeters to feet and inches
+ * Converts inches into centimeters
  */
+
+import java.util.Scanner;
+
+public class Convert
+{
+  public static void main(String[] args)
+  {
+    final double CM_PER_INCH = 2.54;
+    
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("How many inches? ");
+    int inches = input.nextInt();
+    double cm = inches * CM_PER_INCH;
+    System.out.printf(" %d in. =  %.2f cm.\n",inches,cm);
+    
+    
+    
+  }
+  
+  
+  
+  
+}
