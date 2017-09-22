3ffda52e280909110a6c73fda5021386903ee546
Purdy <374266@flourbluffschools.net>
2017-09-22 10:42:45 -0500

    Changed in class

--- 1st-6Weeks/2017-09-20-Input/ConvertInClass.java
+++ 1st-6Weeks/2017-09-20-Input/ConvertInClass.java
@@ -12,8 +12,14 @@ public class ConvertInClass
     
     Scanner input = new Scanner(System.in);
     
+    System.out.print("How many feet? ");
+    int feet = input.nextInt();
+        
     System.out.print("How many inches? ");
     int inches = input.nextInt();
+    
+    inches = inches + feet * 12;
+    
     double cm = inches * CM_PER_INCH;
     System.out.printf(" %d in. =  %.2f cm.\n",inches,cm);
     
