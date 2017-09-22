108eed6000997e71548caa88e44ec9e7d967992d
Purdy <374266@flourbluffschools.net>
2017-09-22 10:52:03 -0500

    added challenges folder

---
+++ 1st-6Weeks/Challenges/FourFours.java
@@ -0,0 +1,39 @@
+public class FourFours
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Edit the following so that it prints out true statements.");
+    System.out.println("One catch: You can only use four 4s and the following\n" 
+                         + "arithmetic operators:\n"
+                         + " +, - , *, /, Math.pow()\n"
+                         + "Math.pow(a,b) is equivalent to a^b in most calculators.\n\n"
+                         + "The first one is done for you as an example.");
+    int zero   = 4 + 4 - 4 - 4;
+    int one    = 4444;
+    int two    = 4444;
+    int three  = 4444;
+    int four   = 4444;
+    int five   = 4444;
+    int six    = 4444;
+    int seven  = 4444;
+    int eight  = 4444;
+    int nine   = 4444;
+    int ten    = 4444;
+    
+    System.out.println("0 = " + zero);
+    System.out.println("1 = " + one);
+    System.out.println("2 = " + two);
+    System.out.println("3 = " + three);
+    System.out.println("4 = " + four);
+    System.out.println("5 = " + five);
+    System.out.println("6 = " + six);
+    System.out.println("7 = " + seven);
+    System.out.println("8 = " + eight);
+    System.out.println("9 = " + nine);
+    System.out.println("10= " + ten);
+
+  }
+}
+      
+                         
+                         
