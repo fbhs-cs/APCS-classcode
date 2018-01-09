b4d3ffb028ea5401cee845878ca33de0a7a2464f
Chad Purdy <cpurdy@flourbluffschools.net>
2018-01-09 08:05:55 -0600

    triples

---
+++ 1st-6Weeks/FirstSemReview/Triples/StarterCode/Triples.java
@@ -0,0 +1,34 @@
+/* Save a copy of this file in the
+ * parent directory (Triples) and
+ * delete this comment.
+ */
+
+public class Triples
+{
+   private int number;
+
+ public Triples()
+ {
+  this.number = 0;
+ }
+
+ public Triples(int num)
+ {
+ }
+
+ public void setNum(int num)
+ {
+ }
+ 
+ private int greatestCommonFactor(int a, int b, int c)
+ {
+  int max = 0;
+  return 1;
+ }
+
+ public String toString()
+ {
+  String output="";
+  return output+"\n";
+ }
+}
---
+++ 1st-6Weeks/FirstSemReview/Triples/StarterCode/TriplesRunner.java
@@ -0,0 +1,22 @@
+/* Save a copy of this file in the
+ * parent directory (Triples) and
+ * delete this comment.
+ */
+
+
+public class TriplesRunner
+{
+  public static void main(String args[])
+  {
+    /*uncomment below this line when your class 
+    is finished.*/
+    
+    //Triples n = new Triples(110);
+    //System.out.println("Pythogorean triples less than 110");
+    //System.out.println(n);
+    //System.out.println();
+    //System.out.println("Pythagorean triples less than 45");
+    //n.setNum(45);
+    //System.out.println(n);
+  }
+}
