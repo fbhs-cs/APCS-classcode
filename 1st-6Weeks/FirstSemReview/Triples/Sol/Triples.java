7308fffb65e75ab1a26c3679866bbc8798990ae3
Chad Purdy <cpurdy@flourbluffschools.net>
2018-01-11 10:58:43 -0600

    triples solution

---
+++ 1st-6Weeks/FirstSemReview/Triples/Sol/Triples.java
@@ -0,0 +1,63 @@
+/* Save a copy of this file in the
+ * parent directory (Triples) and
+ * delete this comment.
+ */
+
+public class Triples
+{
+   private int number;
+
+ public Triples()
+ {
+  this.number = 0;
+ }
+
+ public Triples(int num)
+ {
+   this.number = num;
+ }
+
+ public void setNum(int num)
+ {
+   this.number = num;
+ }
+ 
+ private int greatestCommonFactor(int a, int b, int c)
+ {
+  int max = 0;
+  int min = Math.min(Math.min(a,b),c);
+  for(int i = 1; i < min; i++)
+  {
+    if(a % i == 0 && b % i == 0 && c % i == 0)
+      max = i;
+  }
+  return max;
+ }
+
+ public String toString()
+ {
+  String output="";
+  for(int a = 1; a < this.number; a++)
+  {
+    for(int b = a; b< this.number; b ++)
+    {
+      for(int c = b; c< this.number; c++)
+      {
+        if(c%2 == 0)
+          continue;
+        else if(a*a + b*b == c*c)  
+        {
+          if((a%2==0 && b%2==1) || (a%2==1 && b%2==0))
+          {
+            if (greatestCommonFactor(a,b,c)==1)
+            {
+              output += String.format("%d %d %d\n",a,b,c);
+            }
+          }
+        }
+      }
+    }
+  }
+  return output+"\n";
+ }
+}
