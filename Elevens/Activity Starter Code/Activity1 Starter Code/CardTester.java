59e111ce6fe254758e4574e723ebf509f6449db5
Chad Purdy <chpurdy@gmail.com>
2018-03-18 20:50:21 -0500

    my work

--- Elevens/Activity Starter Code/Activity1 Starter Code/CardTester.java
+++ Elevens/Activity Starter Code/Activity1 Starter Code/CardTester.java
@@ -3,11 +3,21 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+  
+   Card card1 = new Card("Ace","Spades",11);
+   Card card2 = new Card("King","Hearts",10);
+   Card card3 = new Card("8","Clubs",8);
+   
+   System.out.println(card1);
+   System.out.println("card1 == card2:" + card1.equals(card2));
+   System.out.println(card1.rank());
+   System.out.println(card2.suit());
+   System.out.println(card3.pointValue());
+   
+ }
 }
