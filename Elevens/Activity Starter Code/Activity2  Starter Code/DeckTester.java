e9c1df2c49e3feea3da5628e99245242cfddc5cb
Chad Purdy <chpurdy@gmail.com>
2018-03-18 20:50:21 -0500

    my work

--- Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
+++ Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,37 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+ /**
+  * The main method in this class checks the Deck operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+   
+   // Testing Deck
+   String[] ranks = {"jack","queen","king"};
+   String[] suits = {"blue","red"};
+   int[] pointValues = {11,12,13};
+   Deck d = new Deck(ranks,suits,pointValues);
+   System.out.println("Printing Deck...");
+   System.out.println(d);
+   //System.out.println(d.isEmpty());
+   System.out.println("Dealing...");
+   while (!d.isEmpty())
+   {
+     System.out.println(d.deal());
+   }
+   System.out.println("Printing deck...");
+   System.out.println(d);
+   
+   
+   // Twenty One Deck
+   String[] twentyOneRanks = {"Ace","King","Queen","Jack","10","9","8","7","6","5","4","3","2"};
+   String[] twentyOneSuits = {"spades","hearts","clubs","diamonds"};
+   int[] twentyOneValues = {11,10,10,10,10,9,8,7,6,5,4,3,2};
+   
+   Deck twentyOneDeck = new Deck(twentyOneRanks, twentyOneSuits, twentyOneValues);
+   System.out.println(twentyOneDeck);
+  
+ 
+ }
 }
